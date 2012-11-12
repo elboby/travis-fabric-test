25c096534fa7ccab50f7442a2f7b2b8eb90ca2ad
arnaud seilles <arnaud.seilles@gmail.com>
2012-11-12 15:13:56 +0100

    initial commit

---
+++ fabfile.py
@@ -0,0 +1,24 @@
+from fabric.api import *
+
+SERVERS = {}
+SERVERS['aws'] = [
+    'hero@ec2-54-247-135-204.eu-west-1.compute.amazonaws.com'
+]
+SERVERS['localbox'] = [
+    'hero@192.168.3.201'
+]
+SERVERS['travis'] = [
+    'travis@127.0.0.1'
+]
+
+
+@task
+def on_(machine):
+    """ Setup machine environment """
+    env.hosts = SERVERS[machine]
+
+@task
+def test():
+    """ run simple test """
+    local("pwd")
+    run("pwd")
